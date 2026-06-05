72a3886995074f92abb361d89611b01ffda7d9f3
Plum <plum1127@qq.com>
2026-06-05 11:07:34 +0800

    修改lenet为alexnet

--- AlexNet/train.py
+++ AlexNet/train.py
@@ -165,8 +165,8 @@ def matplot_acc_loss(train_process: pd.DataFrame):
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    leNet = AlexNet()
+    alexnet = AlexNet()
 
     train_loader, val_loader = train_val_data_process()
-    train_process = train_model_process(leNet, train_loader, val_loader, 20)
+    train_process = train_model_process(alexnet, train_loader, val_loader, 20)
     matplot_acc_loss(train_process)
